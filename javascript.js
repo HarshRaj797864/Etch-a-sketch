d65c3da04656d624f8a254d75792be889f0df1f7
Harsh Raj <rajharsh797864@gmail.com>
2025-06-28 04:39:30 +0530

    <feat>: Add 2 color modes: Color-mode with color-picker and Rainbow-mode

--- javascript.js
+++ javascript.js
@@ -9,6 +9,46 @@ for (let i = 0; i < gridSize * gridSize; i++) {
 
 }
 
+// Default Configurations 
+let currentColor = '#8AC686'
+const modes = {
+    COLOR: 'color',
+    RAINBOW: 'rainbow'
+};
+let currentMode = modes.COLOR;
+
+const colorPicker = document.querySelector('#drawing-color');
+colorPicker.addEventListener("input", (e) => {
+    currentColor = e.target.value;
+});
+
+const colorModeButton = document.querySelector('#color-mode');
+const rainbowModeButton = document.querySelector('#rainbow-mode');
+
+colorModeButton.addEventListener("click", () => {
+    currentMode = modes.COLOR;
+    colorPicker.disabled = false;
+    updateActiveModeButtons();
+});
+
+rainbowModeButton.addEventListener("click", () => {
+    currentMode = modes.RAINBOW;
+    colorPicker.disabled = true;
+    updateActiveModeButtons();
+});
+
+function updateActiveModeButtons() {
+    colorModeButton.classList.toggle('active', currentMode === modes.COLOR);
+    rainbowModeButton.classList.toggle('active', currentMode === modes.RAINBOW);
+}
+
+updateActiveModeButtons();
+
+function getRainbowColor() {
+    const hue = Math.floor(Math.random() * 360);
+    return `hsl(${hue}, 100%, 50%)`;
+}
+
 let isMouseDown = false;
 document.addEventListener("mouseup", () => {
     isMouseDown = false;
@@ -22,15 +62,16 @@ let cells = Array.from(CELLS)
 cells.forEach((cell) => {
     cell.addEventListener("mousedown", () => {
         isMouseDown = true;
-        cell.setAttribute("style", "background-color: red")
+        cell.style.backgroundColor = currentMode === modes.COLOR? currentColor : getRainbowColor();
     });
 
     cell.addEventListener("mouseover", () => {
-        if (isMouseDown === true) {
-            cell.setAttribute("style", "background-color: red");
+        if (isMouseDown) {
+            cell.style.backgroundColor = currentMode === modes.COLOR? currentColor : getRainbowColor();
         }
 
     });
 
 });
+
 
