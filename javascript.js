a5390d755de6234d9f47ee08f5117d33422058bd
Harsh Raj <rajharsh797864@gmail.com>
2025-06-28 05:06:14 +0530

    <feat>: Add 2 new modes: clear and erase modes

--- javascript.js
+++ javascript.js
@@ -9,11 +9,17 @@ for (let i = 0; i < gridSize * gridSize; i++) {
 
 }
 
+let CELLS = document.querySelectorAll(".cell");
+// Convert NodeList to Array to have more methods
+let cells = Array.from(CELLS)
+
 // Default Configurations 
 let currentColor = '#8AC686'
 const modes = {
     COLOR: 'color',
-    RAINBOW: 'rainbow'
+    RAINBOW: 'rainbow',
+    ERASER: 'eraser',
+    CLEAR: 'clear'
 };
 let currentMode = modes.COLOR;
 
@@ -24,6 +30,8 @@ colorPicker.addEventListener("input", (e) => {
 
 const colorModeButton = document.querySelector('#color-mode');
 const rainbowModeButton = document.querySelector('#rainbow-mode');
+const eraserButton = document.querySelector('#erase-mode');
+const clearButton = document.querySelector('#clear-mode');
 
 colorModeButton.addEventListener("click", () => {
     currentMode = modes.COLOR;
@@ -37,9 +45,28 @@ rainbowModeButton.addEventListener("click", () => {
     updateActiveModeButtons();
 });
 
+eraserButton.addEventListener("click", () => {
+    currentMode = modes.ERASER;
+    colorPicker.disabled = true;
+    updateActiveModeButtons();
+})
+
+function clearGrid() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.style.backgroundColor = '';
+    });
+    currentMode = modes.COLOR;
+}
+
+clearButton.addEventListener("click", () => {
+    clearGrid();
+})
+
 function updateActiveModeButtons() {
     colorModeButton.classList.toggle('active', currentMode === modes.COLOR);
     rainbowModeButton.classList.toggle('active', currentMode === modes.RAINBOW);
+    eraserButton.classList.toggle('active', currentMode === modes.ERASER);
 }
 
 updateActiveModeButtons();
@@ -54,20 +81,32 @@ document.addEventListener("mouseup", () => {
     isMouseDown = false;
 });
 
-
-let CELLS = document.querySelectorAll(".cell");
-// Convert NodeList to Array to have more methods
-let cells = Array.from(CELLS)
-
 cells.forEach((cell) => {
     cell.addEventListener("mousedown", () => {
         isMouseDown = true;
-        cell.style.backgroundColor = currentMode === modes.COLOR? currentColor : getRainbowColor();
+         
+        if (currentMode === modes.RAINBOW) {
+            cell.style.backgroundColor = getRainbowColor();
+        }
+        else if (currentMode === modes.COLOR) {
+            cell.style.backgroundColor = currentColor;
+        }
+        else {
+            cell.style.backgroundColor = "white"; 
+        }
     });
 
     cell.addEventListener("mouseover", () => {
         if (isMouseDown) {
-            cell.style.backgroundColor = currentMode === modes.COLOR? currentColor : getRainbowColor();
+            if (currentMode === modes.RAINBOW) {
+                cell.style.backgroundColor = getRainbowColor();
+            }
+            else if (currentMode === modes.COLOR) {
+                cell.style.backgroundColor = currentColor;
+            }
+            else {
+                cell.style.backgroundColor = "white"; 
+            }
         }
 
     });
